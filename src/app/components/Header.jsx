65d8cb92cdfda197f58eafec1fa894b4f607bf82
Adnan Ghani <agadnanrocks07@gmail.com>
2025-05-08 20:47:09 +0530

    Added next-theme

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,57 +1,65 @@
-'use client';
-import { Button, Navbar, TextInput } from 'flowbite-react';
-import Link from 'next/link';
-import { AiOutlineSearch } from 'react-icons/ai';
-import { FaMoon } from 'react-icons/fa';
-import { usePathname } from 'next/navigation';
+"use client";
+import { Button, Navbar, TextInput } from "flowbite-react";
+import Link from "next/link";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon, FaSun } from "react-icons/fa";
+import { usePathname } from "next/navigation";
+import { useTheme } from "next-themes";
+
 export default function Header() {
   const path = usePathname();
+  const { theme, setTheme } = useTheme();
   return (
-    <Navbar className='border-b-2'>
+    <Navbar className="border-b-2">
       <Link
-        href='/'
-        className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
+        href="/"
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
       >
-        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
           Adnan's
         </span>
         Blog
       </Link>
       <form>
         <TextInput
-          type='text'
-          placeholder='Search...'
+          type="text"
+          placeholder="Search..."
           rightIcon={AiOutlineSearch}
-          className='hidden lg:inline'
+          className="hidden lg:inline"
         />
       </form>
-      <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
         <AiOutlineSearch />
       </Button>
-      <div className='flex gap-2 md:order-2'>
-        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
-          <FaMoon />
+      <div className="flex gap-2 md:order-2">
+        <Button
+          className="w-12 h-10 hidden sm:inline"
+          color="gray"
+          pill
+          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+        >
+          {theme === "dark" ? <FaSun /> : <FaMoon />}
         </Button>
-        <Link href='/sign-in'>
-          <Button gradientDuoTone='purpleToBlue' outline>
+        <Link href="/sign-in">
+          <Button gradientDuoTone="purpleToBlue" outline>
             Sign In
           </Button>
         </Link>
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-        <Link href='/'>
-          <Navbar.Link active={path === '/'} as={'div'}>
+        <Link href="/">
+          <Navbar.Link active={path === "/"} as={"div"}>
             Home
           </Navbar.Link>
         </Link>
-        <Link href='/about'>
-          <Navbar.Link active={path === '/about'} as={'div'}>
+        <Link href="/about">
+          <Navbar.Link active={path === "/about"} as={"div"}>
             About
           </Navbar.Link>
         </Link>
-        <Link href='/projects'>
-          <Navbar.Link active={path === '/projects'} as={'div'}>
+        <Link href="/projects">
+          <Navbar.Link active={path === "/projects"} as={"div"}>
             Projects
           </Navbar.Link>
         </Link>
