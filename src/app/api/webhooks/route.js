64e956b21c36c89566c67025ff6d6d8925c80652
Adnan Ghani <agadnanrocks07@gmail.com>
2025-05-09 17:58:57 +0530

    Updated route.js

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -1,4 +1,4 @@
-import { clerkClient } from "@clerk/nextjs/dist/types/server";
+import { clerkClient } from "@clerk/nextjs";
 import { verifyWebhook } from "@clerk/nextjs/webhooks";
 import { deleteUser } from "../actions/user";
 import { createOrUpdateUser } from "../actions/user";
